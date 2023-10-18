f9ca6e5584ec8ef94b86581b54a8282570e54e2f
nitishkalra <nitishkalra1998@gmail.com>
2023-10-18 14:44:05 +0530

    add loading feature

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, createContext, useContext,  } from 'react'
-import { Outlet, redirect, useLoaderData, useNavigate } from 'react-router-dom'
+import { Outlet, redirect, useLoaderData, useNavigate, useNavigation } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/Dashboard';
-import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import { BigSidebar, Navbar, SmallSidebar, Loading} from '../components';
 import { checkDefaultTheme } from '../App';
 import customFetch from '../utils/customFetch.js';
 import { toast } from 'react-toastify';
@@ -21,6 +21,8 @@ const DashboardContext = createContext();
 const DashboardLayout = ({isDarkThemeEnabled}) => {
   const user = useLoaderData();
   const navigate = useNavigate();
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
   
@@ -48,7 +50,7 @@ const DashboardLayout = ({isDarkThemeEnabled}) => {
           <div>
             <Navbar />
             <div className='dashboard-page'>
-              <Outlet context={{user}}/>
+              {isPageLoading ? <Loading /> : <Outlet context={{user}}/>}
             </div>
           </div>
         </main>
